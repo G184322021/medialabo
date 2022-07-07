2373831a13a9a9aa3027bfcd8ea8aff30ae10adf
G184322021 <218432@st.tajushoku-u.ac.jp>
2022-07-07 15:22:52 +0900

    5回

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -7,14 +7,13 @@ let kaisu = 0;
 
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
-hantei();
-hantei();
-hantei();
-hantei();
+let b1 = document.querySelector('button#print');
+b1.addEventListener('click', hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-    let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
+    let b2 = document.querySelector('input[name="kazu"]');
+    let yoso =Number(b2.value);       // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
